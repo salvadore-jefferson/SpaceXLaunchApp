88d55bbdb3bb020a200033538f26c57c24d9b60a
Salvadore Jefferson <salailama1@gmail.com>
2018-12-08 17:35:47 -0600

    added features

---
+++ public/precache-manifest.98d691973c1e6796dd20f7451971bb01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5d65c0163dc9bfb5f5edf66fa084b5c",
+    "url": "/static/media/spacex-logo.f5d65c01.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab683de7066b8b53715b",
+    "url": "/static/js/main.ab683de7.chunk.js"
+  },
+  {
+    "revision": "ee21caea66d5d526cf17",
+    "url": "/static/js/1.ee21caea.chunk.js"
+  },
+  {
+    "revision": "ab683de7066b8b53715b",
+    "url": "/static/css/main.ac38e6cd.chunk.css"
+  },
+  {
+    "revision": "d7f578db63d1a51a946df3b0a9f05442",
+    "url": "/index.html"
+  }
+];
